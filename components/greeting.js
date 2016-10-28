152c554cbcf652b02341a429a314b6a61bab9667
Herman <leonardo.herman@gmail.com>
2016-10-28 01:01:46 -0300

    fixed indentation

--- components/greeting.js
+++ components/greeting.js
@@ -2,39 +2,39 @@ import React from 'react'
 import css from 'next/css'
 
 export default () => (
-	<div className={container}>
-		<h1 className={title}>
-			Hi, I'm<br />
-			<span>Evil Rabbit</span>
-		</h1>
-		<h2 className={subtitle}>Designer from Buenos Aires</h2>
-	</div>
+  <div className={container}>
+    <h1 className={title}>
+      Hi, I'm<br />
+      <span>Evil Rabbit</span>
+    </h1>
+    <h2 className={subtitle}>Designer from Buenos Aires</h2>
+  </div>
 )
 
 let container = css({
-	position: 'absolute',
-	textAlign: 'center',
-	top: '45%',
+  position: 'absolute',
+  textAlign: 'center',
+  top: '45%',
   transform: 'translateY(-50%)',
-	width: '100%'
+  width: '100%'
 })
 
 let title = css.merge(
-	{
-		fontSize: '100px',
-		fontWeight: 100,
-		margin: '0 0 20px 0',
-	},
-	css.media('(max-width: 500px)', {
-		fontSize: '64px'
-	}),
-	css.$(' span', {
-		fontWeight: 500
-	})
+  {
+    fontSize: '100px',
+    fontWeight: 100,
+    margin: '0 0 20px 0',
+  },
+  css.media('(max-width: 500px)', {
+    fontSize: '64px'
+  }),
+  css.$(' span', {
+    fontWeight: 500
+  })
 )
 
 let subtitle = 	css({
-	fontSize: '18px',
-	fontWeight: 100,
-	margin: 0
+  fontSize: '18px',
+  fontWeight: 100,
+  margin: 0
 })
--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,17 @@ import Greeting from '../components/greeting'
 import Logo from '../components/logo'
 
 export default () => (
-	<div className={container}>
-		<Head>
-			<title>Evil Rabbit</title>
-			<link rel="shortcut icon" href="static/favicon.png" />
-			<meta name="viewport" content="initial-scale=1.0" />
-			<meta name="theme-color" content="black" />
-		</Head>
-		<Navbar />
-		<Greeting />
-	  <Logo />
-	</div>
+  <div className={container}>
+    <Head>
+      <title>Evil Rabbit</title>
+      <link rel="shortcut icon" href="static/favicon.png" />
+      <meta name="viewport" content="initial-scale=1.0" />
+      <meta name="theme-color" content="black" />
+    </Head>
+    <Navbar />
+    <Greeting />
+    <Logo />
+  </div>
 )
 
 css.insertRule('html, body { background: black }')
@@ -27,13 +27,13 @@ let fadeIn = css.keyframes({
 })
 
 let container = css({
-	animation: `${fadeIn} 2s`,
-	position: 'absolute',
-	overflow: 'hidden',
-	top: 0,
-	bottom: 0,
-	left: 0,
-	right: 0,
-	color: 'white',
-	fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif'
+  animation: `${fadeIn} 2s`,
+  position: 'absolute',
+  overflow: 'hidden',
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  color: 'white',
+  fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif'
 })
